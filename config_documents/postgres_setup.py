12c757c4a22938d39ef9c4f829f31bbe21f2357b
Luisgar0821 <luis_arcia@DESKTOP-9N3CR7L.>
2025-04-10 16:04:55 -0500

    finish EDA's

--- config_documents/postgres_setup.py
+++ config_documents/postgres_setup.py
@@ -55,6 +55,37 @@ def crear_tabla_grammy_awards(conn):
     cur.close()
     print("Tabla 'grammy_awards' verificada/creada.")
 
+def crear_tabla_spotify_data(conn):
+    cur = conn.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS spotify_data (
+            id SERIAL PRIMARY KEY,
+            artists TEXT,
+            album_name TEXT,
+            track_name TEXT,
+            popularity INTEGER,
+            duration_ms INTEGER,
+            explicit BOOLEAN,
+            danceability FLOAT,
+            energy FLOAT,
+            key INTEGER,
+            loudness FLOAT,
+            mode INTEGER,
+            speechiness FLOAT,
+            acousticness FLOAT,
+            instrumentalness FLOAT,
+            liveness FLOAT,
+            valence FLOAT,
+            tempo FLOAT,
+            time_signature INTEGER,
+            track_genre TEXT
+        );
+    """)
+    conn.commit()
+    cur.close()
+    print("Tabla 'spotify_data' verificada/creada.")
+
+
 def insertar_datos_grammy_awards(conn, df):
     cur = conn.cursor()
     cur.execute("SELECT COUNT(*) FROM grammy_awards")
@@ -73,6 +104,46 @@ def insertar_datos_grammy_awards(conn, df):
             ))
         conn.commit()
         print("Datos insertados en 'grammy_awards'")
+    else:
+        print("Los datos ya están insertados. No se duplicaron registros.")
+    cur.close()
+
+def insertar_datos_spotify_data(conn,df):
+    cur = conn.cursor()
+    cur.execute("SELECT COUNT(*) FROM spotify_data")
+    if cur.fetchone()[0] == 0:
+        for _, row in df.iterrows():
+            cur.execute("""
+                INSERT INTO spotify_data (
+                    artists, album_name, track_name, popularity,
+                    duration_ms, explicit, danceability, energy, key,
+                    loudness, mode, speechiness, acousticness, instrumentalness,
+                    liveness, valence, tempo, time_signature, track_genre
+                ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,
+                          %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """, (
+                row['artists'],
+                row['album_name'],
+                row['track_name'],
+                int(row['popularity']),
+                int(row['duration_ms']),
+                bool(row['explicit']),
+                float(row['danceability']),
+                float(row['energy']),
+                int(row['key']),
+                float(row['loudness']),
+                int(row['mode']),
+                float(row['speechiness']),
+                float(row['acousticness']),
+                float(row['instrumentalness']),
+                float(row['liveness']),
+                float(row['valence']),
+                float(row['tempo']),
+                int(row['time_signature']),
+                row['track_genre']
+            ))
+        conn.commit()
+        print("Datos insertados en 'spotify_data'.")
     else:
         print("Los datos ya están insertados. No se duplicaron registros.")
     cur.close()
